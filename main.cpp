cdcbd046dc8003886a4bb5c5ffd26a58c46a6d23
MihirRao01 <mihirhrao@gmail.com>
2022-10-29 18:44:44 -0400

    added page rank function

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+#include <iterator>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <iomanip>
+
+using namespace std;
+#include "Graph.h"
+
+using namespace std;
+
+
+int main() 
+{ 
+    // int no_of_lines, power_iterations; 
+    // std::string from, to; 
+    // std::cin >> no_of_lines; 
+    // std::cin >> power_iterations; 
+    // for(int i = 0; i < no_of_lines; i++) { 
+    //     std::cin >> from; 
+    //     std::cin >> to; 
+    //     // Do Something 
+    // } 
+    // //Create a graph object 
+    // //Created_Graph.PageRank(power_iterations); 
+
+    AdjacencyList graph;
+
+
+
+
+
+} 
